05df764e0bb0a958bff20029668f5df1db9e4387
Werik Rodrigues <51803873+wrksystem@users.noreply.github.com>
2025-03-08 16:29:12 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# the_tab_news
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/br/com/wrksystem/the_tab_news/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.wrksystem.the_tab_news
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
